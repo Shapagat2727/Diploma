1355c5f6cd5367cb0f05f82e57b8b0f75c95d13f
shapagat <shapagat2727@mail.ru>
2020-04-01 15:38:31 +0600

    Changed midtermVC to catalogVC

--- Diploma/Controllers_Student/CatalogViewController.swift
+++ Diploma/Controllers_Student/CatalogViewController.swift
@@ -1,5 +1,5 @@
 //
-//  TestsViewController.swift
+//  CatalogViewController.swift
 //  Diploma
 //
 //  Created by Шапагат on 2/13/20.
@@ -7,53 +7,67 @@
 //
 
 import UIKit
-
+import RealmSwift
 import ChameleonFramework
 
-class TestsViewController: UIViewController {
+class CatalogViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
-    let testLabelArray = ["Midterm1", "Midterm2", "Final"]
+    let realm = try! Realm()
+    var courses:Results<Course>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.dataSource = self
+        loadCourses()
         tableView.delegate = self
+        tableView.dataSource = self
         tableView.separatorStyle = .none
         tableView.register(UINib(nibName: K.topicNibName, bundle: nil), forCellReuseIdentifier: K.topicCell)
         
     }
-    
+    func loadCourses(){
+        courses = realm.objects(Course.self)
+        self.tableView.reloadData()
+    }
     
     
     
 }
 //MARK:-TableView DataSource Methods
-extension TestsViewController: UITableViewDataSource{
+extension CatalogViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testLabelArray.count
+        return courses?.count ?? 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.topicCell, for: indexPath) as! TopicCell
-        cell.topicLabel?.text = testLabelArray[indexPath.row]
-        if let color = FlatWatermelon().darken(byPercentage: CGFloat(indexPath.row)/CGFloat(testLabelArray.count)){
+        cell.topicLabel?.text = courses?[indexPath.row].name ?? "No courses added yet"
+        
+        if let color = FlatWatermelon().darken(byPercentage: CGFloat(indexPath.row)/CGFloat(courses?.count ?? 1)){
             cell.topicBubble?.backgroundColor = color
             cell.topicLabel?.textColor = ContrastColorOf(backgroundColor: color, returnFlat: true)
         }
         return cell
+        
     }
-                                   
+    
     
 }
 //MARK:-TableView Delegate Methods
-extension TestsViewController: UITableViewDelegate{
-   
+extension CatalogViewController: UITableViewDelegate{
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 230;
+        return K.largeCell;
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: K.questionSegue, sender: self)
+        registerAlert(with: courses?[indexPath.row].name ?? "Course Name")
+    }
+    func registerAlert(with selectedCourse: String){
+        let alert = UIAlertController(title: selectedCourse, message: "Want to register to this course?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Register", style: .default, handler:nil))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
     }
 }
+
+
